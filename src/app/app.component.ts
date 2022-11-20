5d75e603a0979b4af683a579a9c4d934f4f54fd4
Diana Kozachenko <kozachenkodiana03@mail.ru>
2022-11-20 20:14:34 +0700

    Code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
 import { Component } from '@angular/core';
 
+type ThemeType = 'light' | 'dark';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'theme-switcher';
+  /**
+   * Метод смены темы с помощью CSS-переменных
+   * @param {ThemeType} theme - тема
+   * @memberof AppComponent
+   */
+  public cssVarsMethod(theme: ThemeType): void {
+    document.body.style.setProperty('--background-color-main', `var(--background-color-${theme})`);
+    document.body.style.setProperty('--title-color-main', `var(--title-color-${theme})`)
+    document.body.style.setProperty('--text-color-main', `var(--text-color-${theme})`)
+  }
+
+  /**
+   * Метод смены темы с помощью добавления класса
+   * @memberof AppComponent
+   */
+  public classMethod(): void {
+    const element: HTMLElement | null = document.querySelector(':root');
+    element?.classList.toggle('dark');
+  }
+
+  /**
+   * Метод смены темы с помощью инвертирования цветов
+   * @memberof AppComponent
+   */
+  public invertMethod(): void {
+    const element: HTMLElement | null = document.querySelector(':root');
+    element?.classList.toggle('invert');
+  }
 }
